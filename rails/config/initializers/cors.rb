a2472fe4fb0877781f02c2a9ee0397a0aa70adb1
Munetaka <sakuraapp1988@gmail.com>
2021-04-17 11:56:16 +0900

    Add rack-cors

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # フロントの開発環境は localhost:3000 にて動作させるため、 CROS の設定を行う
+  # 基本的には同一オリジンによるリクエストのため、本番時には不要となる想定
+  allow do
+    origins 'localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
