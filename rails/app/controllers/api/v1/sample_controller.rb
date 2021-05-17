1e3203a4e72d4190530266f8372fcaecb57069e4
Munetaka <sakuraapp1988@gmail.com>
2021-05-17 22:03:14 +0900

    [WIP] Rails の cookie / Modelの生成など

--- rails/app/controllers/api/v1/sample_controller.rb
+++ rails/app/controllers/api/v1/sample_controller.rb
@@ -2,7 +2,26 @@ module Api
   module V1
     class SampleController < ApplicationController
       def index
-        render json: { status: 'SUCCESS', message: 'success' }
+        # user_name が存在しなければ、cookie に david を付与して render
+        unless cookies[:account_id]
+          cookies.signed[:account_id] = {
+            value: 1,
+            httponly: true,
+            secure: true,
+            expires: 2.hours.from_now,
+            same_site: :strict
+          }
+
+          return render json: { status: 'SUCCESS', message: 'success' }
+        end
+
+        # model側の処理
+        @account = Account.find_by(account_id: cookies.signed[:account_id])
+
+        # cookie を削除する場合は、以下のような記載を行う
+        # value = cookies.signed[:value]
+        # cookies.delete(:account_id)
+        render json: { status: 'SUCCESS', message: 'your david', value: @account.account_name }
       end
     end
   end
